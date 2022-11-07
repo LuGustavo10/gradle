324f017d1d296f73cbce72336cb516aa1f91023f
Luís Gustavo <108032070+LuGustavo10@users.noreply.github.com>
2022-11-06 23:02:12 -0300

    Add files via upload

---
+++ projetoJAVA_FX/app/src/main/java/helloworldwithgradle/LoginScene.java
@@ -0,0 +1,27 @@
+package helloworldwithgradle;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class LoginScene extends Scene {
+	
+	Label lbUserName = new Label("Nome do Usuario:");
+	TextField txtUserName = new TextField("Digite o username aqui:");
+
+	Label lbUserPass = new Label("Senha:");
+	TextField txtUserPass = new TextField("Digite a senha aqui...");
+
+	public LoginScene(VBox vbox, double width, double height) {
+		super(vbox, width, height);
+		
+		vbox.getChildren().add(lbUserName);
+		vbox.getChildren().add(txtUserName);
+
+		vbox.getChildren().add(lbUserPass);
+		vbox.getChildren().add(txtUserPass);
+	}
+
+}
+
